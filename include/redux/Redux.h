f2829ab943119ad4e1692ff40804cd258ccbe21c
Ashwin Kamath <ashwinkn@hotmail.com>
2020-08-12 13:19:24 -0500

    Added combineReducers

--- include/redux/Redux.h
+++ include/redux/Redux.h
@@ -3,36 +3,75 @@
 #ifndef __BST_REDUX_H__
 #define __BST_REDUX_H__
 
+#include <functional>
 #include <list>
+#include <map>
 #include <string>
 #include <type_traits>
-#include <unordered_map>
+#include <utility>
 
 namespace bst::redux {
 
-template <typename State, typename Application> class Store {
+template <typename State, typename Action> class Store {
+  public:
+    using Reducer = std::function<State(const State &, const Action &)>;
 
   private:
     State state;
+    Reducer reducer;
 
   public:
     void setState(const State &newState) { state = newState; }
     const State getState() const { return state; }
 
-    template <typename Action> void dispatch(const Action &&action)
+    void dispatch(const Action &action)
     {
-        const auto newState = Application()(getState(), action);
+        const auto newState = reducer(getState(), action);
         setState(newState);
     };
 
-    Store(const State &s) : state(s) { ; }
+    Store(const State &s, const Reducer &a) : state(s), reducer(a) { ; }
 };
 
-template <typename State, typename Application>
-auto createStore(const State &init, const Application &application)
+template <typename State, typename Action>
+auto createStore(const State &init,
+                 const typename Store<State, Action>::Reducer &reducer)
 {
-    class Store<State, Application> store(init);
+    class Store<State, Action> store(init, reducer);
     return store;
+}
+
+template <typename State, typename Action>
+auto combineReducers(
+    const std::map<std::string,
+                   std::function<typename State::mapped_type(
+                       const typename State::mapped_type &, const Action &)>>
+        &&reducers)
+{
+    return [reducers = std::move(reducers)](const State &state,
+                                            const Action &action) {
+        State newState;
+        bool changed = false;
+
+        for (auto &pair : reducers) {
+            auto &key = pair.first;
+            typename State::mapped_type oldValue;
+
+            try {
+                oldValue = state.at(key);
+            }
+            catch (std::out_of_range) {
+            }
+
+            auto &&newValue = pair.second(oldValue, action);
+            if (newValue != oldValue) {
+                newState.insert(std::pair(key, newValue));
+                changed = true;
+            }
+        }
+
+        return changed ? newState : state;
+    };
 }
 
 } // namespace bst::redux
--- redux/tests/ReduxStoreTest.cc
+++ redux/tests/ReduxStoreTest.cc
@@ -1,4 +1,7 @@
 
+#include <string>
+#include <unordered_map>
+
 #include "redux/Redux.h"
 #include "gtest/gtest.h"
 
@@ -6,14 +9,14 @@ using namespace bst::redux;
 
 TEST(ReduxStoreTest, ReduxStoreLifecycle)
 {
-    auto store =
-        createStore(0, [](int oldState, std::string action) { return 0; });
+    auto store = createStore<int, std::string>(
+        0, [](int oldState, std::string action) { return 0; });
 }
 
 TEST(ReduxStoreTest, ReduxStoreDispatch)
 {
-    auto store =
-        createStore(0, [](int oldState, int action) { return 0xfeef; });
+    auto store = createStore<int, int>(
+        0, [](int oldState, int action) { return 0xfeef; });
     store.dispatch(0);
     EXPECT_EQ(0xfeef, store.getState());
 }
@@ -24,7 +27,21 @@ TEST(ReduxStoreTest, ReduxStoreDispatchClass)
         int operator()(int, int) { return 0xfeef; }
     };
 
-    auto store = createStore(0, Application());
+    auto store = createStore<int, int>(0, Application());
     store.dispatch(0);
     EXPECT_EQ(0xfeef, store.getState());
+}
+
+TEST(ReduxStoreTest, CombineReducers)
+{
+    using State = std::unordered_map<std::string, int>;
+    auto application = combineReducers<State, int>({
+        {"return_1", [](const int &oldState, const int action) { return 1; }},
+        {"return_2", [](const int &oldState, const int action) { return 2; }},
+    });
+
+    auto store = createStore<State, int>({}, application);
+    store.dispatch(0);
+    EXPECT_EQ(store.getState().at("return_1"), 1);
+    EXPECT_EQ(store.getState().at("return_2"), 2);
 }
